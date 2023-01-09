27925df1235257766c48b729377a64fb235ac421
Leothurm <leonardo.thurm@gmail.com>
2023-01-09 11:57:49 -0300

    JavaScript parte 3 - correção de bugs

--- script.js
+++ script.js
@@ -37,17 +37,19 @@ function iniciar() {
     document
       .getElementById("timer")
       .style.setProperty("display", "block", "important");
+
+      PLAY()
   }
 }
 
-function play() {
+function PLAY() {
   let valueSessions = localStorage.getItem("sessions");
 
   if (valueSessions != "1") {
-    document.getElementById("title_sessao").innerHTML += " sessões restantes";
-  } else {
-    document.getElementById("title_sessao").innerHTML += " sessão restante";
-  }
+    document.getElementById('title_sessao').innerHTML = sessions.value + ' sessões restantes'
+} else {
+    document.getElementById('title_sessao').innerHTML = sessions.value + ' sessão restante'
+}
 
   let title = document.getElementById("title");
   title.innerHTML = "ACAO";
@@ -88,5 +90,54 @@ function play() {
   }
 }
 
+function momentoPausa() {
+  let title = document.getElementById("title");
+  title.innerHTML = "PAUSA";
+  title.style.fontSize = "30px";
+  title.style.fontWeight = "bold";
+  title.style.setProperty("color", "#dc3545", "important");
 
+  min_pausa = Number(localStorage.getItem("pausa"));
+  min_pausa -= 1;
+  segundos = 59;
 
+  document.getElementById('minutes_ok').innerHTML = min_pausa
+  document.getElementById('seconds_ok').innerHTML = segundos
+
+  var min_interval = setInterval(minTimer, 60000)
+  var seg_interval = setInterval(segTimer, 1000)
+
+  function minTimer() {
+    min_pausa -= 1
+    document.getElementById('minutes_ok').innerHTML = min_pausa
+ }
+
+ function segTimer(){
+    segundos = segundos - 1
+   document.getElementById('seconds_ok').innerHTML = segundos
+
+   if (segundos <= 0) {
+      if (min_pausa <= 0) {
+         ses = Number(localStorage.getItem('sessions'))
+         ses -= 1
+         localStorage.setItem('sessoes', String(ses))
+         clearInterval(min_interval)
+         clearInterval(seg_interval)
+
+         if (ses <= 0) {
+            final.play()
+            localStorage.clear()
+
+            document.getElementById('config').style.setProperty('display', 'none', 'important')
+            document.getElementById('timer').style.setProperty('display', 'none', 'important')
+            document.getElementById('fim').style.setProperty('display', 'block', 'important')
+         } else {
+            volta.play();
+            PLAY()
+        }
+    }
+    segundos = 60
+ }
+}
+
+}
