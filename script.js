55b43666488b836c5baf78a4e0e4d815d403f591
Leothurm <leonardo.thurm@gmail.com>
2023-01-09 11:30:59 -0300

    JavaScript parte 2

--- script.js
+++ script.js
@@ -1,30 +1,92 @@
-let acao = document.getElementById('acao')
-let pausa = document.getElementById('pausa')
-let sessions = document.getElementById('sessions')
-let segundos
+let acao = document.getElementById("acao");
+let pausa = document.getElementById("pausa");
+let sessions = document.getElementById("sessions");
+let segundos;
 
-var bell = new Audio("./audio/bell.mp3")
-var volta = new Audio("./audio/volta.mp3")
-var final = new Audio("./audio/final.mp3")
+var bell = new Audio("./audio/bell.mp3");
+var volta = new Audio("./audio/volta.mp3");
+var final = new Audio("./audio/final.mp3");
 
-var lofi = document.getElementById('lofi')
-var pause = document.getElementById('pause')
-var play = document.getElementById('play')
+var lofi = document.getElementById("lofi");
+var pause = document.getElementById("pause");
+var play = document.getElementById("play");
 
+function iniciar() {
+  if (acao.value == 0) {
+    document.getElementById("action_error").innerHTML = "Adicione os minutos";
+    acao.focus();
+  } else if (pausa.value == 0) {
+    document.getElementById("pause_error").innerHTML =
+      "Adicione o tempo de pause";
+    pausa.focus();
+  } else if (sessions.value == 0) {
+    document.getElementById("sessions_error").innerHTML =
+      "Adicione o número de sessões";
+    sessions.focus();
+  } else {
+    lofi.play();
+    pause.style.setProperty("display", "block", "important");
 
-function iniciar(){
-    if(acao.value == 0){
-        document.getElementById('action_error').innerHTML = "Adicione os minutos"
-        acao.focus();
-    } else if(pausa.value == 0){
-        document.getElementById('pause_error').innerHTML = "Adicione o tempo de pause"
-        pausa.focus();
-    } else if(sessions.value == 0){
-        document.getElementById('sessions_error').innerHTML = "Adicione o número de sessões"
-        sessions.focus();
-    } else {
-        lofi.play()
-        pause.style.setProperty('display','block','important')
-    }
+    localStorage.setItem("acao", String(acao.value));
+    localStorage.setItem("pausa", String(pausa.value));
+    localStorage.setItem("sessions", String(sessions.value));
 
+    document
+      .getElementById("config")
+      .style.setProperty("display", "none", "important");
+    document
+      .getElementById("timer")
+      .style.setProperty("display", "block", "important");
+  }
 }
+
+function play() {
+  let valueSessions = localStorage.getItem("sessions");
+
+  if (valueSessions != "1") {
+    document.getElementById("title_sessao").innerHTML += " sessões restantes";
+  } else {
+    document.getElementById("title_sessao").innerHTML += " sessão restante";
+  }
+
+  let title = document.getElementById("title");
+  title.innerHTML = "ACAO";
+  title.style.fontSize = "30px";
+  title.style.fontWeight = "bold";
+  title.style.setProperty("color", "#FFF", "important");
+
+  min = Number(localStorage.getItem("acao"));
+  min -= 1;
+  segundos = 59;
+
+  document.getElementById("minutes_ok").innerHTML = min;
+  document.getElementById("seconds_ok").innerHTML = segundos;
+
+  var min_interval = setInterval(minTimer, 60000);
+  var seg_interval = setInterval(segTimer, 1000);
+
+  function minTimer() {
+    min -= 1;
+    document.getElementById("minutes_ok").innerHTML = min;
+  }
+
+  function segTimer() {
+    segundos -= 1;
+    document.getElementById("seconds_ok").innerHTML = segundos;
+
+    if (segundos <= 0) {
+      if (min <= 0) {
+        clearInterval(min_interval);
+        clearInterval(seg_interval);
+
+        bell.play();
+
+        momentoPausa();
+      }
+      segundos = 60;
+    }
+  }
+}
+
+
+
